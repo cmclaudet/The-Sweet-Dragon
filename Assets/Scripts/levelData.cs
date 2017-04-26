f6b467570ba21a8e71022755a085f6dcb3b73a7a
Catherine Claudet <cmclaudet@gmail.com>
2017-04-26 19:41:21 +0200

    Calculated grid points based on grid size and screen size

--- Assets/Scripts/levelData.cs
+++ Assets/Scripts/levelData.cs
@@ -6,6 +6,9 @@ public class levelData : MonoBehaviour {
 	public Grid gridSize;
 	public float spawnMoveSpeed;
 
+	public float[] xGridCoords{get; private set;}
+	public float[] yGridCoords{get; private set;}
+
 	[System.Serializable]
 	public struct Grid {
 		public int x;
@@ -14,6 +17,29 @@ public class levelData : MonoBehaviour {
 		public Grid(int width, int height) {
 			x = width;
 			y = height;
+		}
+	}
+
+	void Start() {
+		setXGridCoords();
+		setYGridCoords();
+	}
+
+	void setXGridCoords() {
+		float gridWidthScreen = Screen.width/gridSize.x;
+		xGridCoords = new float[gridSize.x + 1];
+		for (int i = 0; i <= gridSize.x; i++) {
+			float gridScreenXPos = i*gridWidthScreen;
+			xGridCoords[i] = Camera.main.ScreenToWorldPoint(new Vector3(gridScreenXPos, 0)).x;
+		}
+	}
+
+	void setYGridCoords() {
+		float gridHeightScreen = Screen.height/(gridSize.y + 1);
+		yGridCoords = new float[gridSize.y + 1];
+		for (int i = 0; i <= gridSize.y; i++) {
+			float gridScreenYPos = Screen.height - i*gridHeightScreen;
+			yGridCoords[i] = Camera.main.ScreenToWorldPoint(new Vector3(0, gridScreenYPos)).y;
 		}
 	}
 
