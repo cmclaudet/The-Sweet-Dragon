5515feec6384a7fc2b4d61ce90b191ccf6689575
Catherine Claudet <cmclaudet@gmail.com>
2017-04-26 14:22:15 +0200

    Rearranged functions

--- Assets/dragOnHold.cs
+++ Assets/dragOnHold.cs
@@ -7,18 +7,24 @@ public class dragOnHold : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.touchCount > 0) {
-        if (Input.GetTouch(0).phase == TouchPhase.Began) {
+			detectInitialTouch();
+			dragObject();
+		}
+	}
+
+	void detectInitialTouch() {
+		if (Input.GetTouch(0).phase == TouchPhase.Began) {
             RaycastHit2D hitInfo = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position), Vector2.zero);
-            // RaycastHit2D can be either true or null, but has an implicit conversion to bool, so we can use it like this
             if(hitInfo)
             {
 				if (hitInfo.transform.gameObject == gameObject) {
                 	dragging = true;
-                	// Here you can check hitInfo to see which collider has been hit, and act appropriately.
 				}
             }
         }
+	}
 
+	void dragObject() {
 		if (dragging) {
 			Vector3 fingerPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
 			transform.position = new Vector3 (fingerPos.x, fingerPos.y, transform.position.z);
@@ -26,6 +32,5 @@ public class dragOnHold : MonoBehaviour {
 				dragging = false;
 			}
 		}
-	}
 	}
 }
