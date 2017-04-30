29fb312eaa4c2fcf13071a63a41771c9989c02ee
Catherine Claudet <cmclaudet@gmail.com>
2017-04-30 19:15:58 +0200

    Add scrolling script to background

---
+++ Assets/Scripts/scrolling.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scrolling : MonoBehaviour {
+
+    public float speed = 0.4f;
+
+    // Make texture on quad repeat itself for infinitely scrolling background
+    void Update()
+    {
+        Vector2 offset = new Vector2(0, Time.time * speed);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
+}
