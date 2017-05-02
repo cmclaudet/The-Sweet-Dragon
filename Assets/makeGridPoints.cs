2719d7bb403565b4b629d306d6a3aa070dd820a8
Catherine Claudet <cmclaudet@gmail.com>
2017-05-02 10:35:45 +0200

    Grid row prefab made
    - Create gird row prefab which creates grid point objects long it based
    on lane number
    - Spawn either sweet or rock at one of the grid point objects

---
+++ Assets/makeGridPoints.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class makeGridPoints : MonoBehaviour {
+	[HideInInspector]public int laneNumber;
+	[HideInInspector]public float rockSpawnChance;
+	public Transform sweetPrefab;
+	public Transform rockPrefab;
+	// Use this for initialization
+	void Start () {
+		float gridCellWidthScreen = Screen.width/(float)laneNumber;
+		for (int i = 0; i < laneNumber; i++) {
+			float gridScreenXPos = (i+0.5f)*gridCellWidthScreen;
+			float gridWorldXPos = Camera.main.ScreenToWorldPoint(new Vector3(gridScreenXPos, 0)).x;
+			GameObject newGridPoint = new GameObject();
+			newGridPoint.transform.SetParent(transform);
+			newGridPoint.transform.localPosition = new Vector3(gridWorldXPos, 0);
+		}
+		spawnRowObject();
+	}
+	
+	void spawnRowObject() {
+		Transform newObject;
+		if (Random.Range(0,1f) < rockSpawnChance) {
+			newObject = Instantiate(rockPrefab);
+		} else {
+			newObject = Instantiate(sweetPrefab);
+		}
+		setObjectLane(newObject);
+	}
+
+	void setObjectLane(Transform laneObject) {
+		int objectLane = Random.Range(0, laneNumber);
+		foreach (Transform gridPoint in transform) {
+			if (gridPoint.GetSiblingIndex() == objectLane) {
+				laneObject.SetParent(gridPoint);
+				break;
+			}
+		}
+	}
+}
